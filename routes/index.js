d49551f8eb368f81b68b1cec87e14f447e6c18c9
thamaranth <thamaranth@gmail.com>
2016-10-03 17:29:44 -0700

    added single page book functionality

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,15 @@ router.get('/', ( request, response ) => {
   Book.getAll().then( books => response.render('index', { books }))
 });
 
+router.get('/book/:book_id', ( request, response ) => {
+  const { book_id } = request.params
+
+  Book.getOne( book_id )
+    .then( book => {
+
+      response.render( 'book_details', { book }  )
+    })
+    .catch( error => { message: error.message } )
+});
+
 module.exports = router;

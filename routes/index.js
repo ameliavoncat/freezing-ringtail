1b9e0dc8c7fede3bf386ae931766fb5d65a7cde0
Amelia Clarke <ameliavoncat@gmail.com>
2016-10-05 20:09:43 -0700

    Added admin page and routes

--- routes/index.js
+++ routes/index.js
@@ -29,6 +29,10 @@ router.get('/book/:book_id', ( request, response ) => {
     })
 });
 
+router.get('/admin', ( request, response ) => {
+  response.render( 'admin' )
+} )
+
 /* GET create author page. */
 router.get( '/admin/author/create', ( request, response ) => {
   response.render( 'create_author' )
